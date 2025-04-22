2aa5b46947fc6f3851273df87347915cb15338a2
joriel <johannoriel6@gmail.com>
2025-04-22 19:25:28 +0200

    Template for widgets

---
+++ widgets/template.py
@@ -0,0 +1,22 @@
+from lib.global_vars import translations, t
+from app import Widget
+import streamlit as st
+from lib.global_vars import translations, t
+
+translations["en"].update({
+    "template_string1": "In english",
+})
+
+translations["fr"].update({
+    "template_string1": "En français",
+})
+
+
+class VideoDatabaseWidget(Widget):
+    def __init__(self, name, prefix, plugin_manager):
+        super().__init__(name, prefix, plugin_manager)
+
+    def display(self):
+        st.title(t("template_string1"))
+        work_directory = self.plugin_manager.config["common"]["work_directory"]
+        st.input(t("template_string2"), key=f"{self.prefix}_input")
